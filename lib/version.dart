9a38d7b5a643f825a4deb1c056261cf2440e61a4
yooyi <2668760098@qq.com>
2025-02-28 16:52:31 +0800

    new:主题颜色设置

--- lib/version.dart
+++ lib/version.dart
@@ -1,4 +1,4 @@
 // Generated file - DO NOT EDIT. The information in this file may be out of date.
-const String gitCommit = 'cdecd1c';
-const String buildTime = '2025-02-24 11:34:34';
-const String appVersion = '0.8.8';
+const String gitCommit = 'ce06bbc';
+const String buildTime = '2025-02-28 16:49:59';
+const String appVersion = '0.8.12-beta';
