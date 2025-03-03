8141d49ed4662fa60b02d51e9c5645e1b6a41ae4
yooyi <2668760098@qq.com>
2025-03-03 13:01:01 +0800

    feat:不打包安卓x86

--- lib/version.dart
+++ lib/version.dart
@@ -1,4 +1,4 @@
 // Generated file - DO NOT EDIT. The information in this file may be out of date.
-const String gitCommit = '9a38d7b';
-const String buildTime = '2025-03-01 13:20:13';
-const String appVersion = '0.8.12-beta';
+const String gitCommit = 'a836db1';
+const String buildTime = '2025-03-03 13:20:35';
+const String appVersion = '0.8.13-beta';
